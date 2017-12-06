fae2843fabc6a3f46a52a5a8561f52658a983f6d
Tim Filer <tim.j.filer@gmail.com>
2017-12-06 13:19:25 -0600

    Assignment Completed

---
+++ js/app.js
@@ -0,0 +1,58 @@
+(function(){
+	'use strict';
+
+	angular.module('ShoppingListCheckOff',[])
+	.controller('ToBuyController', ToBuyController)
+	.controller('AlreadyBoughtController', AlreadyBoughtController)
+	.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+	ToBuyController.$inject = ['ShoppingListCheckOffService'];
+	AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+
+	function ToBuyController(ShoppingListCheckOffService){
+		var toBuy = this;
+		
+		toBuy.itemsToBuy = ShoppingListCheckOffService.getItemsToBuy();
+
+		toBuy.buyItem = function (itemIndex) {
+			ShoppingListCheckOffService.buyItem(itemIndex);
+		}
+
+	}
+
+	function AlreadyBoughtController(ShoppingListCheckOffService){
+		var alreadyBought = this;
+
+		alreadyBought.itemsBought = ShoppingListCheckOffService.getItemsBought();
+
+	}
+
+	function ShoppingListCheckOffService(){
+		var service = this;
+
+		service.itemsToBuy = []; 
+		service.itemsToBuy.push({ name: "cookies", quantity: 10 } ); 
+		service.itemsToBuy.push({ name: "sandwiches", quantity: 5 } );
+		service.itemsToBuy.push({ name: "cans of coke", quantity: 4 } );
+		service.itemsToBuy.push({ name: "bags of chips", quantity: 2 } );
+		service.itemsToBuy.push({ name: "trash bags", quantity: 8 } );;
+
+		service.itemsBought = [];
+
+		service.getItemsToBuy = function(){
+			return service.itemsToBuy;
+		}
+
+		service.getItemsBought = function(){
+			return service.itemsBought;
+		}
+
+		service.buyItem = function(itemIdex){
+			service.itemsBought.push(service.itemsToBuy[itemIdex]);
+			service.itemsToBuy.splice(itemIdex,1);
+		}
+
+	}
+
+
+})();
